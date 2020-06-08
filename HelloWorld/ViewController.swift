bfe05e83ac9676e653719e310e7280aebbf34390
Jin Kim <jinkim1195@gmail.com>
2020-06-08 14:24:28 -0700

    Initial commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jin Kim on 6/8/20.
+//  Copyright © 2020 Jin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
